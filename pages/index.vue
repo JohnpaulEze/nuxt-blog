ed9fe4c369867477cd44374073922cb1b0a1b9ec
Johnpaul Eze <ezejohnpaul846@gmail.com>
2023-02-23 00:47:06 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid h-screen place-content-center gap-3">
+    <h1 class="text-5xl text-blue-500">Welcome to Nuxt Content</h1>
+    <nuxt-link
+      href="/blog"
+      class="mx-auto rounded-xl bg-amber-700 px-20 py-4 text-white"
+    >
+      Go to Blog
+    </nuxt-link>
+  </div>
+</template>
